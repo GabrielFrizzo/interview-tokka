7399283e5e3ef5e00a5bdad13942e9764d981ff8
Gabriel Frizzo <gabrielfrizzo@gmail.com>
2024-03-10 11:08:30 +0800

    Run initial pre-commit

--- tests/test_etherscan_client.py
+++ tests/test_etherscan_client.py
@@ -5,6 +5,7 @@ from etherscan.client import EtherscanClient
 
 def test_get_token_transactions() -> None:
     client = EtherscanClient("IHF6CDIVFN731EBFCFKFTZE3XYRKUKHXSN")
-    response = client.get_token_transactions("0x88e6a0c2ddd26feeb64f039a2c41296fcb3f5640", page_size=10)
+    response = client.get_token_transactions(
+        "0x88e6a0c2ddd26feeb64f039a2c41296fcb3f5640", page_size=10
+    )
     pprint(response)
-
